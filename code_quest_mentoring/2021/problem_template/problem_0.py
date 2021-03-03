3482e8c548e243c9aefa8ee66bfb1c9e1ff0d152
Alan Yeung <yeungalan0@gmail.com>
2021-03-03 14:51:03 +0000

    Updated with more recent example

--- code_quest_mentoring/2021/problem_template/problem_0.py
+++ code_quest_mentoring/2021/problem_template/problem_0.py
@@ -1,23 +1,17 @@
-import os 
+import sys
+import math
+import string
 
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-INPUT_FILE = DIR_PATH + "/input.txt"
+# Actual solution logic should go here
+def solve(case):
+    print(case)
 
-def code_quest(number_string):
-    number = int(number_string)
-    if number % 21 == 0:
-        print("CODEQUEST")
-    elif number % 7 == 0:
-        print("QUEST")
-    elif number % 3 == 0:
-        print("CODE")
-    else:
-        print(number)
-
+# Logic to read to input and pass to our solving function
 def main():
-    with open(INPUT_FILE) as f:
-        for line in f:
-            code_quest(line.strip())
+    cases = int(sys.stdin.readline().rstrip())
+    for case_num in range(cases):
+        case = sys.stdin.readline().rstrip()
+        solve(case)
 
 if __name__ == "__main__":
     main()
--- code_quest_mentoring/2021/problem_template/test_problem_0.py
+++ code_quest_mentoring/2021/problem_template/test_problem_0.py
@@ -1,14 +1,32 @@
 import pytest
 import problem_0
+import io
+import os
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 expected_output = None
+input = None
 
-with open('expected_output.txt', 'r') as file:
+# Read in our expected output
+with open(f"{DIR_PATH}/expected_output.txt", 'r') as file:
     expected_output = file.read().rstrip("\n")
 
-def test_main(capsys):
+# Read in our expected input
+with open(f"{DIR_PATH}/input.txt", 'r') as file:
+    input = file.read()
+
+def test_main(capsys, monkeypatch):
+    # Mock stdin to pass in our input
+    monkeypatch.setattr('sys.stdin', io.StringIO(input))
+
+    # Call our main function to ultimately solve the problem
     problem_0.main()
+
+    # Capture the program output from stdout/stderr
     actual_output = capsys.readouterr().out.rstrip("\n")
+
+    # Ensure the actual output matches the expected output
     print(f"Actual output:\n{actual_output}\n")
     print(f"Expected output:\n{expected_output}")
     assert actual_output == expected_output
