172cc21465078beafcc48846e57de9801383649a
Alan Yeung <yeungalan0@gmail.com>
2021-03-05 15:47:50 +0000

    minor edit

--- code_quest_mentoring/2021/problem_template/problem.py
+++ code_quest_mentoring/2021/problem_template/problem.py
@@ -2,15 +2,13 @@ import sys
 import math
 import string
 
+
 # Actual solution logic should go here
-
-
 def solve(case):
     print(case)
 
+
 # Logic to read to input and pass to our solving function
-
-
 def main():
     cases = int(sys.stdin.readline().rstrip())
     for case_num in range(cases):
