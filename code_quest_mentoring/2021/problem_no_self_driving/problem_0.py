899b20e38bae33026e765beedcdf047bc366da2d
Alan Yeung <yeungalan0@gmail.com>
2021-03-05 00:42:56 +0000

    Implemented alternative try/except approach Shoc suggested

--- code_quest_mentoring/2021/problem_no_self_driving/problem_0.py
+++ code_quest_mentoring/2021/problem_no_self_driving/problem_0.py
@@ -5,18 +5,22 @@ import string
 # Actual solution logic should go here
 def solve(case):
     velocity, distance = case.split(":")
-    if float(velocity) == 0:
-        print("SAFE")
-        return
+    # if float(velocity) == 0:
+    #     print("SAFE")
+    #     return
 
-    time = float(distance) / float(velocity)
+    try:
+        time = float(distance) / float(velocity)
 
-    if time <= 1:
-        print("SWERVE")
-    elif time <= 5:
-        print("BRAKE")
-    else:
-        print("SAFE")
+        if time <= 1:
+            print("SWERVE")
+        elif time <= 5:
+            print("BRAKE")
+        else:
+            print("SAFE")
+    except ZeroDivisionError as e:
+        print("gtrgrr")
+
 
 # Logic to read to input and pass to our solving function
 def main():
