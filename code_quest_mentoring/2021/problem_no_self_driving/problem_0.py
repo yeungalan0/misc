c614a472c3ae9a63acd45cd5c7b32780a18d362e
Alan Yeung <yeungalan0@gmail.com>
2021-03-05 00:36:33 +0000

    Solved with the Basis class!

---
+++ code_quest_mentoring/2021/problem_no_self_driving/problem_0.py
@@ -0,0 +1,29 @@
+import sys
+import math
+import string
+
+# Actual solution logic should go here
+def solve(case):
+    velocity, distance = case.split(":")
+    if float(velocity) == 0:
+        print("SAFE")
+        return
+
+    time = float(distance) / float(velocity)
+
+    if time <= 1:
+        print("SWERVE")
+    elif time <= 5:
+        print("BRAKE")
+    else:
+        print("SAFE")
+
+# Logic to read to input and pass to our solving function
+def main():
+    cases = int(sys.stdin.readline().rstrip())
+    for case_num in range(cases):
+        case = sys.stdin.readline().rstrip()
+        solve(case)
+
+if __name__ == "__main__":
+    main()
