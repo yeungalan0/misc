32fb9ee70d0fe60ccb771a16b8568925905a0bac
Alan Yeung <yeungalan0@gmail.com>
2021-03-26 00:39:22 +0000

    Coding with basis!

---
+++ code_quest_mentoring/2021/problem_conways_game_of_life/problem.py
@@ -0,0 +1,55 @@
+import sys
+import math
+import string
+
+# Actual solution logic should go here
+ALIVE = 1
+
+
+def solve(iterations, grid):
+    print(f"Iterations: {iterations}")
+    print(f"Grid: {len(grid)}")
+    int_grid = convert_to_int_grid(grid)
+    print(count_adjacent_neighbors(3, 5, int_grid))
+
+
+def convert_to_int_grid(grid):
+    int_grid = []
+    for line in grid:
+        int_grid.append([int(character) for character in line])
+    return int_grid
+
+
+def count_adjacent_neighbors(row, col, grid):
+    neighbors = 0
+    min_row = max(0, row - 1)
+    max_row = min(10, row + 2)
+    min_col = max(0, col - 1)
+    max_col = min(10, col + 2)
+
+    for check_row in range(min_row, max_row):
+        for check_col in range(min_col, max_col):
+            if check_row == row and check_col == col:
+                continue
+            if grid[check_row][check_col] == ALIVE:
+                neighbors += 1
+            print(check_row, check_col)
+
+    return neighbors
+
+
+# Logic to read to input and pass to our solving function
+def main():
+    cases = int(sys.stdin.readline().rstrip())
+    for _ in range(cases):
+        iterations = int(sys.stdin.readline().rstrip())
+        grid = []
+
+        for i in range(10):
+            grid.append(sys.stdin.readline().rstrip())
+
+        solve(iterations, grid)
+
+
+if __name__ == "__main__":
+    main()
---
+++ code_quest_mentoring/2021/problem_conways_game_of_life/test_problem.py
@@ -0,0 +1,33 @@
+import pytest
+import problem
+import io
+import os
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+expected_output = None
+input = None
+
+# Read in our expected output
+with open(f"{DIR_PATH}/expected_output.txt", 'r') as file:
+    expected_output = file.read().rstrip("\n")
+
+# Read in our expected input
+with open(f"{DIR_PATH}/input.txt", 'r') as file:
+    input = file.read()
+
+
+def test_main(capsys, monkeypatch):
+    # Mock stdin to pass in our input
+    monkeypatch.setattr('sys.stdin', io.StringIO(input))
+
+    # Call our main function to ultimately solve the problem
+    problem.main()
+
+    # Capture the program output from stdout/stderr
+    actual_output = capsys.readouterr().out.rstrip("\n")
+
+    # Ensure the actual output matches the expected output
+    print(f"Actual output:\n{actual_output}\n")
+    print(f"Expected output:\n{expected_output}")
+    assert actual_output == expected_output
