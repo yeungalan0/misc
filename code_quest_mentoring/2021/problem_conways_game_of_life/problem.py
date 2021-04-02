d3de0aaa5fcd85a140c51c68e8d452a1a2c9160d
Alan Yeung <yeungalan0@gmail.com>
2021-04-02 00:41:10 +0000

    Getting farther with Basis coding club!

--- code_quest_mentoring/2021/problem_conways_game_of_life/problem.py
+++ code_quest_mentoring/2021/problem_conways_game_of_life/problem.py
@@ -4,13 +4,39 @@ import string
 
 # Actual solution logic should go here
 ALIVE = 1
+DEAD = 0
 
 
 def solve(iterations, grid):
     print(f"Iterations: {iterations}")
     print(f"Grid: {len(grid)}")
     int_grid = convert_to_int_grid(grid)
-    print(count_adjacent_neighbors(3, 5, int_grid))
+    print("ORIGINAL:")
+    print_grid(int_grid)
+    print("NEW:")
+    print_grid(evolve(int_grid))
+
+
+def evolve(grid):
+    new_grid = copy_grid(grid)
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            living_neighbors = count_adjacent_neighbors(row, col, grid)
+            if grid[row][col] == ALIVE and (
+                    living_neighbors < 2 or
+                    living_neighbors > 3
+            ):
+                new_grid[row][col] = 0
+            if grid[row][col] == DEAD and living_neighbors == 3:
+                new_grid[row][col] = 1
+
+    return new_grid
+
+
+def print_grid(grid):
+    for row in grid:
+        print(row)
 
 
 def convert_to_int_grid(grid):
@@ -23,7 +49,7 @@ def convert_to_int_grid(grid):
 def count_adjacent_neighbors(row, col, grid):
     neighbors = 0
     min_row = max(0, row - 1)
-    max_row = min(10, row + 2)
+    max_row = min(10, row + 2)  # TODO
     min_col = max(0, col - 1)
     max_col = min(10, col + 2)
 
@@ -33,9 +59,16 @@ def count_adjacent_neighbors(row, col, grid):
                 continue
             if grid[check_row][check_col] == ALIVE:
                 neighbors += 1
-            print(check_row, check_col)
 
     return neighbors
+
+
+def copy_grid(grid):
+    grid_clone = []
+    for row in grid:
+        grid_clone.append(row)
+
+    return grid_clone
 
 
 # Logic to read to input and pass to our solving function
