ec11712212f5141b9057439d0067f764b19e58d5
Alan Yeung <yeungalan0@gmail.com>
2021-12-17 10:00:37 -0700

    Trying to do Dijkstra

--- advent_of_code_2021/solutions_day15.py
+++ advent_of_code_2021/solutions_day15.py
@@ -1,32 +1,61 @@
-from typing import List
+from typing import Coroutine, List, Set, Tuple
 from utils import parse_input
-from copy import deepcopy
+from heapq import heappop, heappush
 
 
-def tabulate_min_path(grid: List[List[int]]):
-    memo = deepcopy(grid)
+# def tabulate_min_path(grid: List[List[int]]):
+#     memo = deepcopy(grid)
 
-    memo[0][0] = 0
+#     memo[0][0] = 0
 
-    for r in range(len(memo)):
-        for c in range(len(memo[0])):
-            if r == 0 and c == 0:
-                continue
+#     for r in range(len(memo)):
+#         for c in range(len(memo[0])):
+#             if r == 0 and c == 0:
+#                 continue
 
-            top_val = memo[r-1][c] if r-1 >= 0 else 10000000000000000
-            left_val = memo[r][c-1] if c-1 >= 0 else 10000000000000000
+#             top_val = memo[r-1][c] if r-1 >= 0 else 10000000000000000
+#             left_val = memo[r][c-1] if c-1 >= 0 else 10000000000000000
 
-            memo[r][c] = grid[r][c] + \
-                min(top_val, left_val)
+#             memo[r][c] = grid[r][c] + \
+#                 min(top_val, left_val)
 
-    for row in memo:
-        print(row)
+#     for row in memo:
+#         print(row)
 
-    return memo[-1][-1]
+#     return memo[-1][-1]
+
+
+def djikstras_min_path(grid: List[List[int]]) -> int:
+    MAX_RISK = 100000000000
+    seen: Set[Tuple[int, int]] = set()
+
+    options = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+    h: List[Tuple[int, Tuple[int, int]]] = []
+    heappush(h, (0, (0, 0)))
+    seen.add((0, 0))
+
+    while True:
+        min_risk, coordinate = heappop(h)
+
+        for option in options:
+            next_coordinate = (
+                coordinate[0] + option[0], coordinate[1] + option[1])
+
+            is_valid = 0 <= next_coordinate[0] < len(grid) and 0 <= next_coordinate[1] < len(
+                grid[0]) and next_coordinate not in seen
+
+            if is_valid:
+                next_risk = grid[coordinate[0]][coordinate[1]] + min_risk
+                if next_coordinate[0] == len(grid) - 1 and next_coordinate[1] == len(grid[1]) - 1:
+                    return next_risk
+                else:
+                    seen.add(next_coordinate)
+                    heappush(h, (next_risk, next_coordinate))
 
 
 def solve1(problem_input: List[List[int]]) -> int:
-    return tabulate_min_path(problem_input)
+    return djikstras_min_path(problem_input)
 
 
 def solve2(problem_input: List[List[int]]) -> int:
